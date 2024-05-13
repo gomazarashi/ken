f8b2e7ae10219c2d0be1b28685745560101b0382
gomazarashi <Seto42357@gmail.com>
2024-05-13 09:59:38 +0900

    cpp3を追加

---
+++ cpp3/cpp3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "こんにちは" << endl;
+    cout << "あなたの名前はなんですか？" << endl;
+    cin >> name;
+    cout << "よろしく、" << name << "さん。" << endl;
+}
