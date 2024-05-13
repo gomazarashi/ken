595d328fc85ed50c2b853da5569d71f2e78d7641
gomazarashi <Seto42357@gmail.com>
2024-05-13 15:16:11 +0900

    cpp5を追加

---
+++ cpp5/cpp5.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Neko
+{
+private:
+    string name;
+
+public:
+    Neko(string s)
+    {
+        name = s;
+    }
+    void naku()
+    {
+        cout << "私の名前は" << name << "ですにゃ" << endl;
+    }
+};
+
+int main(){
+    Neko dora("ボス");
+    Neko tama("タマ");
+    cout<<"あなたの名付けた猫がメモリ上に生成されました。"<<endl;
+    cout<<"猫が鳴きます。"<<endl;
+
+    dora.naku();
+    tama.naku();
+}
